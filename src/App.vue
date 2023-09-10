789d80c45f71de57c18c20477fb4523b73512957
johanrdev <johanrdev2@gmail.com>
2023-09-10 20:41:35 +0200

    Create responsive navbar menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <template lang="html">
-  <nav class="bg-slate-700 text-slate-400 px-4 py-2">
-    <div class="max-w-5xl mx-auto flex justify-between items-center">
-      <span class="text-2xl">AppName</span>
-      <button class="border-2 border-slate-500 w-12 h-12 rounded hover:text-slate-200 transition-all">
-        <font-awesome-icon :icon="['fas', 'bars']"></font-awesome-icon>
+  <nav class="bg-slate-700 relative text-slate-400 px-4 py-2">
+    <div class="max-w-5xl mx-auto flex justify-between">
+      <span class="text-2xl select-none inline-flex items-center h-12">AppName</span>
+      <button @click="menu.show = !menu.show"
+        class="border-2 border-slate-500 w-12 h-12 shrink-0 rounded hover:text-slate-200 transition-all md:hidden">
+        <font-awesome-icon icon="bars"></font-awesome-icon>
       </button>
+
+      <ul class="hidden md:flex">
+        <li v-for="link in links" :key="link.id">
+          <a :href="link.path" class="block p-3 hover:bg-slate-600 hover:text-slate-200 transition-all rounded">{{ link.name }}</a>
+        </li>
+      </ul>
     </div>
+    <transition appear name="slide-in">
+      <div class="absolute top-full left-0 right-0 bg-slate-800 p-1" v-if="menu.show">
+        <ul>
+          <li v-for="link in links" :key="link.id">
+            <a :href="link.path" class="block p-3 hover:bg-slate-600 hover:text-slate-200 transition-all rounded">{{ link.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </nav>
 </template>
 <script>
-export default {
+import { ref } from 'vue'
 
+export default {
+  setup() {
+    const menu = ref({
+      show: false
+    })
+
+    const links = ref([
+      { id: 1, name: 'Demo', path: '#' },
+      { id: 2, name: 'About', path: '#' },
+      { id: 3, name: 'Contact', path: '#' }
+    ])
+
+    return {
+      menu,
+      links
+    }
+  }
 }
 </script>
-<style lang="css"></style>
+<style lang="css">
+.slide-in-enter-from,
+.slide-in-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-in-enter-to,
+.slide-in-leave-from {
+  transform: translateX(0);
+}
+
+.slide-in-enter-active,
+.slide-in-leave-active {
+  transition: .35s ease-in-out;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import './style.css'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faB, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faBars)
 
 const app = createApp(App)
+app.component("font-awesome-icon", FontAwesomeIcon)
 app.mount('#app')
-app.component('font-awesome-icon', FontAwesomeIcon)
