dc241f79cc7372da4f97c84c57e3e0ced70ed562
johanrdev <johanrdev2@gmail.com>
2023-09-10 20:17:51 +0200

    Basic navbar layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template lang="html">
-  <div class="bg-blue-500 text-white text-center py-8">
-    Hello World
-  </div>
+  <nav class="bg-slate-700 text-slate-400 px-4 py-2">
+    <div class="max-w-5xl mx-auto flex justify-between items-center">
+      <span class="text-2xl">AppName</span>
+      <button class="border-2 border-slate-500 w-12 h-12 rounded"></button>
+    </div>
+  </nav>
 </template>
 <script>
 export default {
-  
+
 }
 </script>
-<style lang="css">
-  
-</style>
+<style lang="css"></style>
