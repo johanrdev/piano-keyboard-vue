0d66b1cbbf9e181811ecaa1d3a5ee04f8ed56870
johanrdev <johanrdev2@gmail.com>
2023-09-10 20:20:33 +0200

    Install and setup font-awesome icons

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <nav class="bg-slate-700 text-slate-400 px-4 py-2">
     <div class="max-w-5xl mx-auto flex justify-between items-center">
       <span class="text-2xl">AppName</span>
-      <button class="border-2 border-slate-500 w-12 h-12 rounded"></button>
+      <button class="border-2 border-slate-500 w-12 h-12 rounded hover:text-slate-200 transition-all">
+        <font-awesome-icon :icon="['fas', 'bars']"></font-awesome-icon>
+      </button>
     </div>
   </nav>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faB, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBars)
+
+const app = createApp(App)
+app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon)
